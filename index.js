95cf64876234a57d49dd66a909b7c2daec6cb255
Thanh An <nguyenthanhan.binhminh@gmail.com>
2023-03-02 14:58:51 +0700

    Fix cors

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const URL = process.env.MONGO_URL;
 const app = express();
 
 app.use(express.json({ limit: '50mb' }));
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors())
 
 app.use('/api/register', register);
 app.use('/api/login', login);
